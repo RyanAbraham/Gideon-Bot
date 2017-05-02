cf3625099835dc4527b89d3126994d028a56719a
YOUR NAME <ryan_ja@live.ca>
2017-05-01 23:28:02 -0400

    Added deck sample hand dealing

--- modules/deck.js
+++ modules/deck.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
+// TODO: Separate deck visualization and hand dealing into separate commands
 class Deck {
   constructor() {
     this.commands = ["deck"];
@@ -12,7 +13,7 @@ class Deck {
   }
 
   handleMessage(command, parameter, msg) {
-    let response = "Deck placeholder text";
+    let response = "Invalid deck";
     if(parameter === "") {
       response = "Please specify a deck URL!";
       return msg.channel.sendMessage(response);
@@ -23,32 +24,57 @@ class Deck {
     request(parameter, (error, reqResponse, html) => {
       if(!error) {
         let $ = cheerio.load(html);
-        let cards = [];
+        let cards = []; // Element [0] is the amount and [1] is the card name
+        let cardsExpanded = [];
         response = "";
-        // Fetch card quantities
+        /* Fetch card quantities */
         var cardAmounts = $(".deck_overview_card_amount");
         for(var i=0; i<cardAmounts.length; i++){
           var element = cardAmounts.eq(i);
           cards.push([element.text().trim()]);
         }
-        // Fetch card names
+        /* Fetch card names */
         var cardNames = $(".deck_overview_card_name");
         for(i=0; i<cardNames.length; i++){
           element = cardNames.eq(i);
           cards[i].push(element.text().trim());
         }
-        // Build a visualization of the deck as the response
-        cards.sort().reverse().forEach((card) => {
-          response += card[0] + "x " + card[1] + "\n";
+        /* Convert name + quantity into an array of names with duplicates */
+        cards.forEach((card) => {
+          for(i=0; i<card[0]; i++) {
+            cardsExpanded.push(card[1]);
+          }
         });
+        // Generate several random hands from the deck as the response
+        for(i=0; i<5; i++) {
+          response += "Sample Hand " + (i+1) + ": ";
+          response += this.shuffle(cardsExpanded).slice(0,7).sort().join(" | ") + "\n";
+        }
       } else {
         response = "Error fetching URL";
       }
       return msg.channel.sendMessage(response);
     });
-    // Use timeout to wait for all the matching cards to be found
-    //setTimeout(() => {
-    //}, this.timeout);
+  }
+
+  // Knuth Shuffle algorithm
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 }
 
